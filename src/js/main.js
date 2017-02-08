9afab2b76d968e13817e260e2d6aae6feb01892b
rm025953 <robert.marlow@cerner.com>
2017-02-07 18:04:13 -0600

    Adding some bands in a table and then filtering them

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 import css from '../css/main.css';
 import $ from 'jquery';
 
+require("script-loader!../../node_modules/jquery/dist/jquery.min.js");
+require("script-loader!../vendor/js/jquery.filtertable.min.js");
+
 var $stuff = $('.some-content');
 
 $stuff.append('<div class="some-content">Hello, I was appended by jQuery!</div>')
+
+jQuery('.bands').filterTable({minRows: 0});
